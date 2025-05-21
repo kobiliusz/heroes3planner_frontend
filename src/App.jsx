622165cdef99742c9d2c54da812899cd396a428b
kobiliusz <kobiliusz@gmail.com>
2025-05-21 19:30:27 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Hero from "./Hero";
+import City from "./City";
 function App() {
 
 
   return (
     <>
+      <Router>
+        <nav>
+          <Link to="/hero">Hero</Link>
+          <Link to="/city">City</Link>
+        </nav>
 
+        <Routes>
+          <Route path="/hero" element={<Hero />} />
+          <Route path="/city" element={<City />} />
+        </Routes>
+      </Router>
     </>
   )
 }
